65918a8f3b5b3cb714d032a44fabfc566b0bebc5
Hristo Dzhambov <dzhambov@hotmail.com>
2020-03-05 14:52:13 -0800

    add complete tests folowing the red-green-refactor.

--- Scrabble.Tests/ModelTests/ScrabbleScoreTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleScoreTests.cs
@@ -18,6 +18,40 @@ namespace Scrabble.Tests
         {
           Assert.AreEqual(4, Word.GameScore("g" + "d"));
         }
-      
+
+    [TestMethod]
+      public void GameScore_WordScore_5()
+        {
+          Assert.AreEqual(5, Word.GameScore("g" + "d" + "o"));
+        }
+
+    [TestMethod]
+      public void GameScore_WordScore_7()
+        {
+          Assert.AreEqual(7, Word.GameScore("c" + "o" + "p"));
+        }
+
+    [TestMethod]
+      public void GameScore_WordScore_9()
+        {
+          Assert.AreEqual(9, Word.GameScore("h" + "o" + "w"));
+        }
+
+    [TestMethod]
+      public void GameScore_WordScore_10()
+        {
+          Assert.AreEqual(10, Word.GameScore("k" + "e" + "e" + "p"));
+        }
+
+    [TestMethod]
+      public void GameScore_WordScore_12()
+        {
+          Assert.AreEqual(12, Word.GameScore("j" + "o" + "b"));
+        }
+    [TestMethod]
+      public void GameScore_WordScore_22()
+        {
+          Assert.AreEqual(22, Word.GameScore("q" + "u" + "i" + "z"));
+        }
     }
   }
