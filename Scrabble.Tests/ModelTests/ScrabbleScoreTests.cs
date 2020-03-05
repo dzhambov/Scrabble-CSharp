aa24aa7cb658b9873518322e7e62a16e410ae065
Hristo Dzhambov <dzhambov@hotmail.com>
2020-03-05 14:06:07 -0800

    add a passing first test and failing second test.

--- Scrabble.Tests/ModelTests/ScrabbleScoreTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleScoreTests.cs
@@ -8,10 +8,15 @@ namespace Scrabble.Tests
   public class WordTest
   {
     [TestMethod]
-    
       public void GameScore_WordScore_1()
         {
-          Assert.AreEqual(2, Word.GameScore("a"));
+          Assert.AreEqual(2, Word.GameScore("a" + "i"));
+        }
+
+    [TestMethod]
+      public void GameScore_WordScore_2()
+        {
+          Assert.AreEqual(4, Word.GameScore("g" + "d"));
         }
       
     }
