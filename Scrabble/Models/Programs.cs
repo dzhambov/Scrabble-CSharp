72557fe674197d6bb83692e6b7540b692fa10459
Hristo Dzhambov <dzhambov@hotmail.com>
2020-03-05 16:43:55 -0800

    add complete UI and Scrable Score application.

--- Scrabble/Models/Programs.cs
+++ Scrabble/Models/Programs.cs
@@ -5,21 +5,24 @@ namespace Scrabble
 {
   public class Program
   {
-    public void Main()
+    public static void Main()
     {
       Console.Clear();
       Console.ForegroundColor = ConsoleColor.DarkYellow;
       TypeLine("Hello Scrambbler");
       TypeLine("Enter a word if you would like to play");
+      Console.ForegroundColor = ConsoleColor.Red;
       string wordInput = Console.ReadLine();
-      TypeLine($"The word that you entered is {wordInput} and recieves {GameScore} piont in Scrabble");
+      int userScore = Word.GameScore(wordInput);
+      TypeLine($"The word that you entered is {wordInput} and recieves {userScore} piont in Scrabble");
+      Main();
     }
     public static void TypeLine(string input)
       {
       for (int i = 0; i < input.Length; i++)
       {
           Console.Write(input[i]);
-          System.Threading.Thread.Sleep(25);
+          System.Threading.Thread.Sleep(35);
       }
       Console.Write(Environment.NewLine);
       Console.Write(Environment.NewLine);
